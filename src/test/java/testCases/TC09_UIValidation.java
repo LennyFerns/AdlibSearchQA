7562ea05b3120e1c8ad5d09a647227dc1f45fcb6
LennyFerns <lennox.fernandes@gmail.com>
2025-04-06 17:30:34 -0400

    Updated TC09 test to include Applitools region checks

--- src/test/java/testCases/TC09_UIValidation.java
+++ src/test/java/testCases/TC09_UIValidation.java
@@ -1,18 +1,26 @@
 package testCases;
 
 import com.applitools.eyes.selenium.Eyes;
+import com.applitools.eyes.selenium.fluent.Target;
 import com.applitools.eyes.RectangleSize;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.Test;
 
 import testBase.BaseClass;
 import com.aventstack.extentreports.Status;
 
 import java.io.IOException;
+import java.time.Duration;
 
 public class TC09_UIValidation extends BaseClass {
 
-    @Test(groups = {"visual", "ui"})
+    @Test(groups = {"visual", "ui","smoke"})
     public void testSearchPageUI() throws IOException {
 
         WebDriver driver = getDriver();
@@ -28,12 +36,33 @@ public class TC09_UIValidation extends BaseClass {
             //Navigate to Search page
             String appURL = p.getProperty("appURL");
             driver.get(appURL);
+            
+            Thread.sleep(1000);
+            
+         // Click the "Advanced Search" checkbox
+            WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+            WebElement advSearchCheckbox = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//input[@id='advs']")));
+            if (!advSearchCheckbox.isSelected()) {
+                ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", advSearchCheckbox);
+                advSearchCheckbox.click();
+            }
+
 
             //Visual checkpoint with Applitools
             eyes.checkWindow("Search Page UI");
 
             // Take local screenshot (for ExtentReport)
             String screenshotPath = captureScreen("SearchPageUI");
+            
+         //  Specific UI element checks
+            eyes.check("Logo", Target.region(By.cssSelector("img[alt='nopCommerce demo store']")));
+            eyes.check("Search Input", Target.region(By.id("q")));
+            eyes.check("Advanced Search Checkbox", Target.region(By.id("advs")));
+            eyes.check("Category Dropdown", Target.region(By.id("cid")));
+            eyes.check("Sub category Checkbox", Target.region(By.id("isc")));
+            eyes.check("Manufacturer Dropdown", Target.region(By.id("mid")));
+            eyes.check("Search In Description Checkbox", Target.region(By.id("sid")));
+            eyes.check("Search Button", Target.region(By.cssSelector("button.search-button")));
 
             // Log it to ExtentReport
             test = extent.createTest("TC09 - Search Page UI Test");
